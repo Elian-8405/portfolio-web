1eda5d6350ea66da8f038adfac9f99ff3386bd49
Elia Silva <elianribeiro022@gmail.com>
2025-05-23 21:41:39 -0300

    add impromevent on redirect page

--- changelang.js
+++ changelang.js
@@ -5,7 +5,8 @@ const supportedLangague = ['pt',   'en']
 const userLang = navigator.language || navigator.userLanguage;
 
 const langPrefix = userLang.split('-')[0]
+const basePath = window.location.pathname.split('/')[1];
 
 if(langPrefix !== 'pt' && supportedLangague.includes(langPrefix)){
-    window.location.href = '/' + langPrefix + '/'
+   window.location.href = '/' + basePath + '/' + langPrefix + '/'
 }
