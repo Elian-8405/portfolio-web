aa109fe1e6924f7b6f5cf1b8b5af7bff6423e81f
Elia Silva <elianribeiro022@gmail.com>
2025-06-14 10:27:15 -0300

    add project01

--- changelang.js
+++ changelang.js
@@ -7,6 +7,8 @@ const userLang = navigator.language || navigator.userLanguage;
 const langPrefix = userLang.split('-')[0]
 const basePath = window.location.pathname.split('/')[1];
 
+console.log("Aqui:" +  basePath + langPrefix)
+
 if(langPrefix !== 'pt' && supportedLangague.includes(langPrefix)){
    window.location.href = '/' + basePath + '/' + langPrefix + '/'
 }
